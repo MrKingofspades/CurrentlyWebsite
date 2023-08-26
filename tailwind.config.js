1d3cf7975ade6155d51e515cbe257e0283f561a7
unknown <ericvining_70@yahoo.com>
2023-08-25 18:18:34 -0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    fontFamily: {
+      'RobotoCondensed': ['Roboto Condensed'],
+      'Inconsolata': ['Inconsolata'],
+      'Orbit': ['Orbitron'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
